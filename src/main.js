5cebc45d7737779151295fd61dd3a0c777e89d69
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-01-08 15:42:53 +0100

    Mehr Daten hinzugefügt.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
-import { jsonLoader } from "./utils"
+import { jsonLoader } from "./utils";
 
 function render(data, targetElement) {
-    targetElement.innerHTML += `
+  targetElement.innerHTML += `
 <div class="article">
     <h3>${data.title}</h3>
     <div class="description">
         ${data.description}
     </div>
 </div>
-`
+`;
 }
 
 async function main() {
-    const artList = document.getElementById("article-list")
+  const artList = document.getElementById("article-list");
 
-    const data = await jsonLoader("public/level-01.json")
-    for (let i = 0; i < data.length; i++) {
-        render(data[i], artList)
-    }
-
+  const data = await jsonLoader("public/level-02.json");
+  for (let i = 0; i < data.length; i++) {
+    render(data[i], artList);
+  }
 }
 
-document.addEventListener("DOMContentLoaded", main)
+document.addEventListener("DOMContentLoaded", main);
