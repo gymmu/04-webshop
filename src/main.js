2ed7b783191743196f77525c54e21d414c542a41
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-01-08 15:33:46 +0100

    Added data and render.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-function main() {
+import { jsonLoader } from "./utils"
+
+function render(data, targetElement) {
+    targetElement.innerHTML += `
+<div class="article">
+    <h3>${data.title}</h3>
+    <div class="description">
+        ${data.description}
+    </div>
+</div>
+`
+}
+
+async function main() {
+    const artList = document.getElementById("article-list")
+
+    const data = await jsonLoader("public/level-01.json")
+    for (let i = 0; i < data.length; i++) {
+        render(data[i], artList)
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", main)
